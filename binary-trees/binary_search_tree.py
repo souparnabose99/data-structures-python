1055909420ddc51bbdbcc00e9d2548d0411fea89
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-01-28 12:23:44 +0530

    Update binary_search_tree.py

--- binary-trees/binary_search_tree.py
+++ binary-trees/binary_search_tree.py
@@ -32,10 +32,49 @@ class BinarySearchTree:
             else:
                 node.right_child = Node(data, node)
 
+    def traverse(self):
+        if self.root is not None:
+            self.traverse_in_order(self.root)
+
+    def traverse_in_order(self, node):
+        if node.left_child:
+            self.traverse_in_order(node.left_child)
+
+        print("Node Data : ", node.data)
+
+        if node.right_child:
+            self.traverse_in_order(node.right_child)
+
+    def get_max_value(self):
+        if self.root:
+            return self.get_max(self.root)
+
+    def get_max(self, node):
+        if node.right_child:
+            return self.get_max(node.right_child)
+
+        return node.data
+
+    def get_min_value(self):
+        if self.root:
+            return self.get_min(self.root)
+
+    def get_min(self, node):
+        if node.left_child:
+            return self.get_min(node.left_child)
+
+        return node.data
+
 
 bst = BinarySearchTree()
 bst.insert(10)
 bst.insert(35)
 bst.insert(5)
-bst.insert(66)
-
+bst.insert(-10)
+bst.insert(99)
+bst.insert(1000)
+bst.insert(17)
+bst.insert(3)
+bst.traverse()
+print("Max : ", bst.get_max_value())
+print("Min : ", bst.get_min_value())
