7b11ded40b9b087b122f9ab05c8600fad47c3752
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-02-21 11:45:09 +0530

    Add files via upload

---
+++ heap/heap_python.py
@@ -0,0 +1,11 @@
+import heapq
+
+# min heap implementation
+nums = [4, 7, 3, -2, 1, 0]
+heap = []
+
+for value in nums:
+    heapq.heappush(heap, value)
+
+while heap:
+    print(heapq.heappop(heap))
