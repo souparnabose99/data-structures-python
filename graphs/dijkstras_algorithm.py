36521b96c1406cd33b9a7c9a3aecf1723fc37c3c
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-08 10:37:17 +0530

    Add files via upload

---
+++ graphs/dijkstras_algorithm.py
@@ -0,0 +1,25 @@
+
+class Edge:
+
+    def __init__(self, weight, start_vertex, target_vertex):
+        self.weight = weight
+        self.start_vertex = start_vertex
+        self.target_vertex = target_vertex
+
+
+class Vertex:
+
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        # this is the node where we came from in the shortest path
+        self.predecessor = None
+        # this is how we store the children
+        self.adjacency_list = []
+        # this is the minimum distance (shortest path) from the source to target vertex
+        self.min_distance = float('inf')
+
+    # this is how python compares objects
+    # insert objects into heap and compare objects
+    def __lt__(self, other_node):
+        return self.min_distance < other_node.min_distance
