b5ab3816ab5855bfbfa58a0b89c6e1cd3b125a39
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-12 13:28:15 +0530

    Update dijkstras_algorithm.py

--- graphs/dijkstras_algorithm.py
+++ graphs/dijkstras_algorithm.py
@@ -68,6 +68,59 @@ class DijkstrasAlgorithm:
         print("Shortest path to the vertex is : ", vertex.min_distance)
         actual_vertex = vertex
 
-        while actual_vertex.predecessor is not None:
+        while actual_vertex is not None:
             print("Vertex Name: ", actual_vertex.name)
             actual_vertex = actual_vertex.predecessor
+
+
+if __name__ == "__main__":
+    # create vertices
+    node1 = Vertex("A")
+    node2 = Vertex("B")
+    node3 = Vertex("C")
+    node4 = Vertex("D")
+    node5 = Vertex("E")
+    node6 = Vertex("F")
+    node7 = Vertex("G")
+    node8 = Vertex("H")
+
+# create the edges (directed edges)
+    edge1 = Edge(5, node1, node2)
+    edge2 = Edge(8, node1, node8)
+    edge3 = Edge(9, node1, node5)
+    edge4 = Edge(15, node2, node4)
+    edge5 = Edge(12, node2, node3)
+    edge6 = Edge(4, node2, node8)
+    edge7 = Edge(7, node8, node3)
+    edge8 = Edge(6, node8, node6)
+    edge9 = Edge(5, node5, node8)
+    edge10 = Edge(4, node5, node6)
+    edge11 = Edge(20, node5, node7)
+    edge12 = Edge(1, node6, node3)
+    edge13 = Edge(13, node6, node7)
+    edge14 = Edge(3, node3, node4)
+    edge15 = Edge(11, node3, node7)
+    edge16 = Edge(9, node4, node7)
+
+    # handle the neighbors
+    node1.adjacency_list.append(edge1)
+    node1.adjacency_list.append(edge2)
+    node1.adjacency_list.append(edge3)
+    node2.adjacency_list.append(edge4)
+    node2.adjacency_list.append(edge5)
+    node2.adjacency_list.append(edge6)
+    node8.adjacency_list.append(edge7)
+    node8.adjacency_list.append(edge8)
+    node5.adjacency_list.append(edge9)
+    node5.adjacency_list.append(edge10)
+    node5.adjacency_list.append(edge11)
+    node6.adjacency_list.append(edge12)
+    node6.adjacency_list.append(edge13)
+    node3.adjacency_list.append(edge14)
+    node3.adjacency_list.append(edge15)
+    node4.adjacency_list.append(edge16)
+
+    # we just have to run the application
+    algorithm = DijkstrasAlgorithm()
+    algorithm.calculate(node1)
+    algorithm.get_shortest_path(node7)
