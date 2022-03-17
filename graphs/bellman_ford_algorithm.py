bee3167b480ebf9411ec9a758357f765468cd2c5
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-17 11:16:03 +0530

    Update bellman_ford_algorithm.py

--- graphs/bellman_ford_algorithm.py
+++ graphs/bellman_ford_algorithm.py
@@ -76,3 +76,40 @@ if __name__ == "__main__":
     node6 = Node("F")
     node7 = Node("G")
 
+    # Create the edges
+    edge1 = Edge(5, node1, node2)
+    edge2 = Edge(9, node1, node5)
+    edge3 = Edge(4, node2, node5)
+    edge4 = Edge(12, node2, node3)
+    edge5 = Edge(7, node2, node4)
+    edge6 = Edge(3, node3, node4)
+    edge7 = Edge(1, node3, node6)
+    edge8 = Edge(9, node4, node7)
+    edge9 = Edge(6, node5, node3)
+    edge10 = Edge(4, node5, node6)
+    edge11 = Edge(2, node6, node7)
+    edge12 = Edge(6, node7, node3)
+    # edge12 = Edge(-6, node7, node3)
+
+    # Adjacency lists
+    node1.adjacency_list.append(edge1)
+    node1.adjacency_list.append(edge2)
+    node2.adjacency_list.append(edge3)
+    node2.adjacency_list.append(edge4)
+    node2.adjacency_list.append(edge5)
+    node3.adjacency_list.append(edge6)
+    node3.adjacency_list.append(edge7)
+    node4.adjacency_list.append(edge8)
+    node5.adjacency_list.append(edge9)
+    node5.adjacency_list.append(edge10)
+    node6.adjacency_list.append(edge11)
+    node7.adjacency_list.append(edge12)
+
+    # run the algorithm
+    vertices = (node1, node2, node3, node4, node5, node6, node7)
+    edges = (edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11, edge12)
+
+    algorithm = BellmanFordAlgorithm(vertices, edges, node1)
+    algorithm.find_shortest_path()
+    algorithm.get_shortest_path(node7)
+
