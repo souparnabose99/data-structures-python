869352c1b7bd15279118fd9b502bbbcf9e2bc0a7
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-29 23:11:59 +0530

    Update hash_table.py

--- hash-table/hash_table.py
+++ hash-table/hash_table.py
@@ -1,1 +1,26 @@
 
+
+class HashTable:
+
+    def __init__(self):
+        # Based on load factor, we can change the size of the underlying data structure(dynamic resizing)
+        self.capacity = 10
+        self.keys = [None] * self.capacity
+        self.values = [None] * self.capacity
+
+    # Takes key as parameter and calculates hash value based on the key
+    def hash_fucntion(self, key):
+        hash_sum = 0
+        for letter in key:
+            hash_sum = hash_sum + ord(letter)
+
+        return hash_sum % self.capacity
+
+
+if __name__ == "__main__":
+    hash_table = HashTable()
+    print(hash_table.hash_fucntion("Souparna"))
+    print(hash_table.hash_fucntion("Bose"))
+
+# 1
+# 3
