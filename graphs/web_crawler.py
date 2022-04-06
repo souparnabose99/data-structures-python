2673c9374b39d7bb29af1bd46d8fda7089b49bcb
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-06 23:03:11 +0530

    Update web_crawler.py

--- graphs/web_crawler.py
+++ graphs/web_crawler.py
@@ -1,1 +1,36 @@
+import requests
+import re
 
+class WebCrawler:
+
+  def __init__(self):
+    # avoid revisiting same websites
+    self.discovered_websites = []
+
+  def crawl(self, start_url):
+    # Bfs implementation
+    queue = [start_url]
+    self.discovered_websites.append(start_url)
+
+    while queue:
+      actual_url = queue.pop(0)
+      print(actual_url)
+
+      # raw representation of a website or url
+      actual_url_html = self.read_raw_html(actual_url)
+
+      for url in self.get_links_from_html(actual_url_html):
+        if url not in self.discovered_websites:
+          self.discovered_websites.append(url)
+          queue.append(url)
+
+  def get_links_from_html(self, raw_html):
+    return re.findall("https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", raw_html)
+
+  def read_raw_html(self, url):
+    raw_html = ''
+    try:
+      raw_html = requests.get(url).text
+    except Exception as e:
+      pass
+    return raw_html
